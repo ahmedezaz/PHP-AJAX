dbbdb06e0e045ab591dcf935326c4298173bfed2
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-22 00:05:07 +0600

    Update show.php

--- show.php
+++ show.php
@@ -1,1 +1,39 @@
+<?php
 
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxphp') or die("CONNECTION ERRO");
+
+$sql = "SELECT * FROM details";
+
+$result = mysqli_query($conn, $sql) or die("SQL ERROR");
+
+if(mysqli_num_rows($result) > 0){
+
+    $output = '<table>
+                <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Age</th>
+                </tr>
+    ';
+
+    while($row = mysqli_fetch_assoc($result)){
+        $output .= "<tr>
+
+                    <td>{$row["id"]} </td>
+                    <td>{$row["name"]} </td>
+                    <td>{$row["age"]} </td>
+        
+                    </tr>";
+    }
+
+    $output .= '</table>';
+    mysqli_close($conn);
+
+    echo $output;
+
+}else {
+        echo "ERROR";
+}
+
+
+?>
