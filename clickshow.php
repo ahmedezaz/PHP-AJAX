d0d2ee4498bfcc66bbb8b54ef399074dd7412d08
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-22 00:04:28 +0600

    Update clickshow.php

--- clickshow.php
+++ clickshow.php
@@ -1,1 +1,44 @@
 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php AJAX</title>
+</head>
+<body>
+
+<h1 style="text-align:center; background:green;color:#FFFFFF;">PHP AJAX</h1>
+
+<button id="btn">CLICK HERE</button>
+
+
+<div id="show">
+ 
+
+ 
+</div>
+
+<script src="jquery-3.6.0.min.js"></script>
+
+<script>
+
+    $(document).ready(function(){
+        $("#btn").on("click", function(e){
+            $.ajax({
+            url: "show.php",
+            type: "POST",
+            success: function(result){
+
+                $("#show").html(result);
+                
+            }
+        })
+        })
+    })
+
+
+</script>
+    
+</body>
+</html>
