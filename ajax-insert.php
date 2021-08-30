636ca6bce75ecff4d1f543775ce2660936367966
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-30 11:06:23 +0600

    Update ajax-insert.php

--- ajax-insert.php
+++ ajax-insert.php
@@ -1,1 +1,17 @@
+<?php
 
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+
+
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxphp') or die("CONNECTION ERROR");
+
+$sql = "INSERT INTO details(name, age) VALUES ('{$first_name}', '{$last_name}')";
+
+if(mysqli_query($conn, $sql)){
+    echo 1;
+}else{
+    echo 0;
+}
+
+?>
