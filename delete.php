c718402e065ab71546aaf40e6bf0102cb60d91e2
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-31 10:45:24 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,19 @@
+<?php
 
+$studentId = $_POST["id"];
+
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxphp') or die("CONNECTION ERRO");
+
+$sql = "DELETE FROM details WHERE id = {$studentId}";
+
+$result = mysqli_query($conn, $sql) or die("SQL ERROR");
+
+if(mysqli_query($conn, $sql)){
+    echo 1;
+}else{
+    echo 0;
+}
+
+
+
+?>
