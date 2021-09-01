80bdbf45da70d8578f6daf682a406a426aced0e1
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-09-01 10:47:02 +0600

    Update ajax-update-form.php

--- ajax-update-form.php
+++ ajax-update-form.php
@@ -1,1 +1,17 @@
+<?php
 
+$student_id = $_POST["id"];
+$firstName = $_POST["first_name"];
+$age = $_POST["last_age"];
+
+$conn = mysqli_connect("localhost","root","","ajaxphp") or die("Connection Failed");
+
+$sql = "UPDATE details SET name = '{$firstName}',age = '{$age}' WHERE id = {$student_id}";
+
+if(mysqli_query($conn, $sql)){
+  echo 1;
+}else{
+  echo 0;
+}
+
+?>
